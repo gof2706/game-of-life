ba1dd53bd0fe12eefc8d52de4ef48203caa1c9f5
Гофман Федор <bigsky261@gmail.com>
2019-05-15 17:27:20 +0700

    Change clang

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include "game.h"
 using namespace std;
-extern void fillrand(int n, int m, vector<vector<int>>& a);
-extern void logic(int n, int m, vector<vector<int>>& a);
-extern void outp(int n, int m, vector<vector<int>>& a);
+extern void fillrand(int n, int m, vector< vector< int > >& a);
+extern void logic(int n, int m, vector< vector< int > >& a);
+extern void outp(int n, int m, vector< vector< int > >& a);
 
 int main()
 {
     int n = 18, m = 18;
-    vector<vector<int>> a(n, vector<int>(m, 0));
+    vector< vector< int > > a(n, vector< int >(m, 0));
     fillrand(n, m, a);
     int l = 0;
     while (l < 100) {
