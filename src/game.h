e41a20c1333c6d27087cf7ed52ec8a0364de2a6a
Гофман Федор <bigsky261@gmail.com>
2019-05-30 00:22:30 +0700

    Change main.cpp

--- src/game.h
+++ src/game.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #include <fstream>
 #include <iostream>
 #include <stdio.h>
@@ -9,3 +10,20 @@
 #include <vector>
 using namespace std;
 using namespace sf;
+using namespace std;
+extern void fillrand(int n, int m, vector< vector< int > >& a);
+extern void logic(int n, int m, vector< vector< int > >& a);
+extern void fillbill(int n, int m, vector< vector< int > >& a);
+extern void fillpuls(int n, int m, vector< vector< int > >& a);
+extern void fillskre(int n, int m, vector< vector< int > >& a);
+extern void filltumb(int n, int m, vector< vector< int > >& a);
+extern void fillpalk(int n, int m, vector< vector< int > >& a);
+extern void fillprac(int n, int m, vector< vector< int > >& a);
+extern void filldroz(int n, int m, vector< vector< int > >& a);
+extern void
+outp(int n,
+     int m,
+     std::vector< std::vector< int > >& a,
+     RenderWindow& app,
+     Sprite& s,
+     int w);
--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,47 @@
 #include "game.h"
-using namespace std;
-extern void fillrand(int n, int m, vector< vector< int > >& a);
-extern void logic(int n, int m, vector< vector< int > >& a);
-extern void outp(int n, int m, vector< vector< int > >& a);
-extern void fillbill(int n, int m, vector< vector< int > >& a);
-extern void fillpuls(int n, int m, vector< vector< int > >& a);
-extern void filldroz(int n, int m, vector< vector< int > >& a);
-extern void fillpalk(int n, int m, vector< vector< int > >& a);
-extern void fillprac(int n, int m, vector< vector< int > >& a);
-extern void fillskre(int n, int m, vector< vector< int > >& a);
-extern void filltumb(int n, int m, vector< vector< int > >& a);
 
 int main()
 {
-    RenderWindow app(VideoMode(450, 450), "game-of-life");
-
     Texture t;
     t.loadFromFile("../src/img/wb.jpg");
     Sprite s(t);
-
     int w = 15;
-
-    int n = 30, m = 30;
+    int n = 70, m = 40;
+    RenderWindow app(VideoMode(1050, 600), "game-of-life");
     vector< vector< int > > a(n, vector< int >(m, 0));
-    //    fillrand(n, m, a);
-    //	fillbill(n,m,a);
-    //	filldroz(n,m,a);
-    //	fillpalk(n,m,a);
-    //	fillprac(n,m,a);
-    //	fillskre(n,m,a);
-    filltumb(n, m, a);
-    //    fillpuls(n, m, a);
-
-    while (1) {
+    while (app.isOpen()) {
         Event e;
-        if ((e.type == Event::Closed) && (app.pollEvent(e)))
+        if (e.type == Event::Closed)
             app.close();
-        app.clear(Color::White);
-        for (int i = 0; i < 30; i++) {
-            for (int j = 0; j < 30; j++) {
-                if (a[i][j] == 0)
-                    s.setTextureRect(IntRect(0 * w, 0, w, w));
-                else
-                    s.setTextureRect(IntRect(1 * w, 0, w, w));
-                s.setPosition(i * w, j * w);
-                app.draw(s);
-            }
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+            app.close();
         }
-        app.display();
-
+        if (Keyboard::isKeyPressed(Keyboard::R)) {
+            fillrand(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::B)) {
+            fillbill(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::P)) {
+            fillpuls(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::S)) {
+            fillskre(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::T)) {
+            filltumb(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::D)) {
+            filldroz(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::W)) {
+            fillprac(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Q)) {
+            fillpalk(n, m, a);
+        }
+        outp(n, m, a, app, s, w);
         logic(n, m, a);
-        sf::Time t1 = sf::milliseconds(50);
-        sleep(t1);
     }
+    return 0;
 }
