292c12560aaaa8883659e21c40194f5e2a5f12ac
Гофман Федор <bigsky261@gmail.com>
2019-05-30 00:22:02 +0700

    Change outp.cpp

--- src/outp.cpp
+++ src/outp.cpp
@@ -1,10 +1,23 @@
 #include "game.h"
-void outp(int n, int m, std::vector< std::vector< int > >& a)
+void outp(
+        int n,
+        int m,
+        std::vector< std::vector< int > >& a,
+        RenderWindow& app,
+        Sprite& s,
+        int w)
 {
-    for (int i = 0; i < n; i++) {
-        std::cout << std::endl;
+    app.clear(Color::White);
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++) {
-            std::cout << a[i][j] << " ";
+            {
+                if (a[i][j] == 0)
+                    s.setTextureRect(IntRect(0 * w, 0, w, w));
+                else
+                    s.setTextureRect(IntRect(1 * w, 0, w, w));
+                s.setPosition(i * w, j * w);
+                app.draw(s);
+            }
         }
-    }
+    app.display();
 }
