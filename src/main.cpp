38a18407896136024e6171079cd1af5614b8e032
Гофман Федор <bigsky261@gmail.com>
2019-05-29 23:40:42 +0700

    Change main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,14 @@ extern void filltumb(int n, int m, vector< vector< int > >& a);
 
 int main()
 {
+    RenderWindow app(VideoMode(450, 450), "game-of-life");
+
+    Texture t;
+    t.loadFromFile("../src/img/wb.jpg");
+    Sprite s(t);
+
+    int w = 15;
+
     int n = 30, m = 30;
     vector< vector< int > > a(n, vector< int >(m, 0));
     //    fillrand(n, m, a);
@@ -23,12 +31,26 @@ int main()
     //	fillskre(n,m,a);
     filltumb(n, m, a);
     //    fillpuls(n, m, a);
-    int l = 0;
-    while (l < 100) {
-        l++;
-        cout << endl;
-        outp(n, m, a);
+
+    while (1) {
+        Event e;
+        if ((e.type == Event::Closed) && (app.pollEvent(e)))
+            app.close();
+        app.clear(Color::White);
+        for (int i = 0; i < 30; i++) {
+            for (int j = 0; j < 30; j++) {
+                if (a[i][j] == 0)
+                    s.setTextureRect(IntRect(0 * w, 0, w, w));
+                else
+                    s.setTextureRect(IntRect(1 * w, 0, w, w));
+                s.setPosition(i * w, j * w);
+                app.draw(s);
+            }
+        }
+        app.display();
+
         logic(n, m, a);
+        sf::Time t1 = sf::milliseconds(50);
+        sleep(t1);
     }
-    return 0;
 }
