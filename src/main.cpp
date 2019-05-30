71bc906217a57131158f93747ce176b209aa4a1a
Гофман Федор <bigsky261@gmail.com>
2019-05-30 18:37:35 +0700

    Debug in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,36 +10,6 @@ int main()
     RenderWindow app(VideoMode(1050, 600), "game-of-life");
     vector< vector< int > > a(n, vector< int >(m, 0));
     while (app.isOpen()) {
-        Event e;
-        if (e.type == Event::Closed)
-            app.close();
-        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
-            app.close();
-        }
-        if (Keyboard::isKeyPressed(Keyboard::R)) {
-            fillrand(n, m, a);
-        }
-        if (Keyboard::isKeyPressed(Keyboard::B)) {
-            fillbill(n, m, a);
-        }
-        if (Keyboard::isKeyPressed(Keyboard::P)) {
-            fillpuls(n, m, a);
-        }
-        if (Keyboard::isKeyPressed(Keyboard::S)) {
-            fillskre(n, m, a);
-        }
-        if (Keyboard::isKeyPressed(Keyboard::T)) {
-            filltumb(n, m, a);
-        }
-        if (Keyboard::isKeyPressed(Keyboard::D)) {
-            filldroz(n, m, a);
-        }
-        if (Keyboard::isKeyPressed(Keyboard::W)) {
-            fillprac(n, m, a);
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Q)) {
-            fillpalk(n, m, a);
-        }
         outp(n, m, a, app, s, w);
         logic(n, m, a);
     }
--- src/outp.cpp
+++ src/outp.cpp
@@ -1,13 +1,39 @@
 #include "game.h"
-void outp(
-        int n,
-        int m,
-        std::vector< std::vector< int > >& a,
-        RenderWindow& app,
-        Sprite& s,
-        int w)
+void outp(int n,int m,std::vector< std::vector< int > >& a,RenderWindow& app, Sprite& s,int w)
 {
-    app.clear(Color::White);
+  while (app.isOpen()) {
+    sf::Event event;
+    while (app.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        app.close();
+        
+      }
+    }   
+if (Keyboard::isKeyPressed(Keyboard::R)) {
+            fillrand(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::B)) {
+            fillbill(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::P)) {
+            fillpuls(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::S)) {
+            fillskre(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::T)) {
+            filltumb(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::D)) {
+            filldroz(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::W)) {
+            fillprac(n, m, a);
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Q)) {
+            fillpalk(n, m, a);
+        }
+ app.clear(Color::White);
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++) {
             {
@@ -20,4 +46,6 @@ void outp(
             }
         }
     app.display();
+ logic(n, m, a);
+}
 }
